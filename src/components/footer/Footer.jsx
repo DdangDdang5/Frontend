986b5f29be059f0856b4e412d9cad8da45ca18f8
raaang <graytorgb@gmail.com>
2022-08-31 18:04:04 +0900

    feat: Header, Footer View 구현 완료

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Footer = () => {
-	return (
-	<div>Footer</div>
-	)
-}
+  const footerList = ["홈", "검색", "채팅", "마이페이지"];
 
-export default Footer
+  return (
+    <FooterContainer>
+      <FooterItemContainer>
+        {footerList.map((item, idx) => (
+          <FooterIcon key={idx}>
+            <img src="maskable.png" alt={item} />
+            {item}
+          </FooterIcon>
+        ))}
+      </FooterItemContainer>
+    </FooterContainer>
+  );
+};
+
+const FooterContainer = styled.div`
+  width: 100vw;
+  height: 70px;
+  border-top: 1px solid gray;
+
+  position: absolute;
+  bottom: 0;
+`;
+
+const FooterItemContainer = styled.div`
+  width: 90vw;
+  height: 100%;
+  margin: auto;
+
+  display: flex;
+`;
+
+const FooterIcon = styled.div`
+  width: 25%;
+  height: 90%;
+  margin: auto;
+  font-size: small;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 30px;
+    height: 30px;
+    margin-bottom: 5px;
+    object-fit: contain;
+    border-radius: 20px;
+  }
+`;
+
+export default Footer;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const Header = () => {
 	return (
-		<div>Header</div>
+		<HeaderContainer>
+			<HeaderContent>
+				<Logo>땅땅</Logo>
+				<HeaderIconContainer>
+					<img src="maskable.png" alt="search" />
+					<img src="maskable.png" alt="alarm" />
+				</HeaderIconContainer>
+			</HeaderContent>
+		</HeaderContainer>
 	)
 }
+
+const HeaderContainer = styled.div`
+	width: 100vw;
+	height: 70px;
+	position: absolute;
+	top: 0;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const HeaderContent = styled.div`
+	width: 90vw;
+
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const Logo = styled.span`
+	font-size: 28px;
+`;
+
+const HeaderIconContainer = styled.div`
+	width: 100px;
+	
+	display: flex;
+	gap: 20px;
+
+	img {
+		width: 40px;
+		height: 40px;
+		object-fit: contain;
+	}
+`;
 
 export default Header
