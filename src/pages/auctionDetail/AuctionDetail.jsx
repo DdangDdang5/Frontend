da805791e88c588e779309f1fc7c1368f7f33e35
baedollee <baesee0401@gmail.com>
2022-09-02 21:16:03 +0900

    feat : auctionDetail page layout view 완성
    댓글이랑 추가적 디자인 나오면 밑으로 붙이기만 하면 됨!

--- src/pages/auctionDetail/AuctionDetail.jsx
+++ src/pages/auctionDetail/AuctionDetail.jsx
@@ -10,32 +10,41 @@ const AuctionDetail = () => {
   return (
     <AuctionDetailLayout>
       <Header />
-      <ItemImg>{Img}</ItemImg>
 
       <DetailBodyWrap>
-        <DetailBodyTitle>
-          게시글 제목이 들어가야 될 것 같습니다!
-        </DetailBodyTitle>
-        <DetailBodyProfile>
-          <DetailBodyProfileImg>{Img}</DetailBodyProfileImg>
-          <DetailBodyProfileContent>
-            <div>아이유</div>
-            <div>신뢰도</div>
-          </DetailBodyProfileContent>
-        </DetailBodyProfile>
+        <ItemImg>{Img}</ItemImg>
 
-        <DetailBodyTag>
-          <div>글자</div>
-          <div>글자</div>
-        </DetailBodyTag>
+        <DetailBodyContainer>
+          <DetailBodyProfile>
+            <DetailBodyProfileImg>{Img}</DetailBodyProfileImg>
+            <DetailBodyProfileContent>
+              <div className="nickName">아이유</div>
+              <div className="trustCount">신뢰도</div>
+            </DetailBodyProfileContent>
+          </DetailBodyProfile>
 
-        <DetailBodyContent>
-          경매 내용입니다. 글자수 제한이 필요할까요?또 모르지 내 마음이 저
-          날씨처럼 바뀔지 날 나조차 다 알 수 없으니 그게 뭐가 중요하니 지금 네게
-          완전히 푹 빠졌단 게 중요한 거지 아마 꿈만 같겠지만 분명 꿈이 아니야
-          달리 설명할 수 없는 이건 사랑일 거야 방금 내가 말한 감정 감히 의심하지
-          마 그냥 좋다는 게 아냐 What's after 'LIKE'?
-        </DetailBodyContent>
+          <DetailBodyTitle>
+            게시글 제목이 들어가야 될 것 같습니다.
+          </DetailBodyTitle>
+
+          <DetailBodyTag>
+            <div>택배</div>
+            <div>서대문구</div>
+          </DetailBodyTag>
+
+          <DetailBodyContent>
+            경매 내용입니다. 글자수 제한이 필요할까요?또 모르지 내 마음이 저
+            날씨처럼 바뀔지 날 나조차 다 알 수 없으니 그게 뭐가 중요하니 지금
+            네게 완전히 푹 빠졌단 게 중요한 거지 아마 꿈만 같겠지만 분명 꿈이
+            아니야 달리 설명할 수 없는 이건 사랑일 거야 방금 내가 말한 감정 감히
+            의심하지 마 그냥 좋다는 게 아냐 What's after 'LIKE'? 경매
+            내용입니다. 글자수 제한이 필요할까요?또 모르지 내 마음이 저 날씨처럼
+            바뀔지 날 나조차 다 알 수 없으니 그게 뭐가 중요하니 지금 네게 완전히
+            푹 빠졌단 게 중요한 거지 아마 꿈만 같겠지만 분명 꿈이 아니야 달리
+            설명할 수 없는 이건 사랑일 거야 방금 내가 말한 감정 감히 의심하지 마
+            그냥 좋다는 게 아냐 What's after 'LIKE'?
+          </DetailBodyContent>
+        </DetailBodyContainer>
       </DetailBodyWrap>
 
       <DetailFooter>
@@ -55,32 +64,34 @@ const AuctionDetailLayout = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-
   background-color: beige;
+`;
+const DetailBodyWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 70px;
+  height: calc(100vh - 140px);
+  overflow: scroll;
 `;
 const ItemImg = styled.div`
   display: flex;
-  margin-top: 70px;
+  margin-bottom: 20px;
   img {
     width: 100%;
     height: 390px;
   }
 `;
-const DetailBodyWrap = styled.div`
+const DetailBodyContainer = styled.div`
   display: flex;
   flex-direction: column;
-  flex: 1;
-  margin: 20px;
-`;
-
-const DetailBodyTitle = styled.div`
-  display: flex;
-  margin-bottom: 20px;
+  padding: 0px 20px;
 `;
 const DetailBodyProfile = styled.div`
   display: flex;
   flex-direction: row;
-  height: 50px;
+  width: 100%;
+  height: 48px;
+  margin-bottom: 24px;
 `;
 const DetailBodyProfileImg = styled.div`
   display: flex;
@@ -96,19 +107,41 @@ const DetailBodyProfileImg = styled.div`
 const DetailBodyProfileContent = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .nickName {
+    font-size: 16px;
+    font-weight: 700;
+  }
+  .trustCount {
+    font-size: 16px;
+    font-weight: 400;
+  }
+`;
+
+const DetailBodyTitle = styled.div`
+  display: flex;
+  font-size: 20px;
+  font-weight: 700;
+  word-break: break-all;
+  margin-bottom: 16px;
 `;
 const DetailBodyTag = styled.div`
   display: flex;
+  margin-bottom: 16px;
   div {
     display: flex;
-    margin: 10px 5px 20px 0;
-    border: 1px solid red;
     border-radius: 20px;
+    background-color: #dedede;
+    padding: 1px 6px;
+    margin-right: 6px;
   }
 `;
 const DetailBodyContent = styled.div`
   display: flex;
   word-break: break-all;
+  font-size: 20px;
+  height: 100%;
 `;
 
 const DetailFooter = styled.div`
