7fa272a925202e96b85a701569a67e7bb276ec85
baedollee <baesee0401@gmail.com>
2022-09-04 23:02:01 +0900

    modify : auctionDetail 서버 데이터 get 오류 수정중

--- src/components/auction/Auction.jsx
+++ src/components/auction/Auction.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const Auction = ({ data }) => {
